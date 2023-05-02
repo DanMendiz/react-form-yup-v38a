086fe759b215082d6b904867ba6471d5f7a4ff32
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-05-02 21:48:27 +0100

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+// import Controlled from './forms/Controlled';
+import FirstHookForm from './forms/FirstHookForm';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <FirstHookForm />
+    </div>
+  );
+}
+
+export default App;
